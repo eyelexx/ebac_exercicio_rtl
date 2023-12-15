1b3d34a94fe69350abbb079d233717ed2ec99bfc
Douglas Eduardo <84141927+eyelexx@users.noreply.github.com>
2023-12-15 16:02:25 -0300

    Adicionado os testes para os comentarios.

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import Post from '../Post';
+import PostComment from '../PostComments';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
@@ -8,3 +8,28 @@ describe('Teste para o componente PostComment', () => {
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
 });
+
+describe('Teste de criacao de comentarios', () => {
+    it('Postagem de primeiro comentario', () => {
+        render(<Post children={undefined} imageUrl={''} />)
+        const inputComent = screen.getByTestId('comment-textarea')
+        const postButton = screen.getByTestId('comment-button')
+
+        fireEvent.change(inputComent, {
+            target: {
+                value: 'Que legal!'
+            }
+        })
+        fireEvent.click(postButton)
+
+        fireEvent.change(inputComent, {
+            target: {
+                value: 'Aguardando o próximo!'
+            }
+        })
+        fireEvent.click(postButton)
+        expect(screen.getAllByTestId('post-container').length).toBe(2)
+
+
+    })
+})
